5aa9a86b71fc354e1378198911998726156d3c5a
Jonathan Leoba <antoleobaj@gmail.com>
2018-05-09 15:54:15 +0100

    Initial commit

---
+++ assets/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LEOBA
+ * Date: 09/05/2018
+ * Time: 00:02
+ */
+
+require_once '../core/Router.php';
+//
+//echo '<pre>';
+//
+//echo print_r($_SERVER);
+//
+//echo '</pre>';
+//
+
+$router = new \Core\Router();
+
+$router->load();
